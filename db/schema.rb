450efef5971eabb49b65414acc311c54f6a5e356
twingay96 <flavonoid37@gmail.com>
2023-12-28 22:01:45 +0900

    create posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_124348) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_28_130107) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
